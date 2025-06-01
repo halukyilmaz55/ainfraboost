5caff6cdbebbb3e9930c3be1c1fae975357bc0be
Haluk Yilmaz <haluk.yilmaz@roofstacks.com>
2025-06-01 18:52:30 +0300

    fix

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@ import IconFacebook from "@/assets/icons/IconFacebook.svg";
 import IconTelegram from "@/assets/icons/IconTelegram.svg";
 import IconPinterest from "@/assets/icons/IconPinterest.svg";
 import IconYouTube from "@/assets/icons/IconYouTube.svg";
+
 import { SITE } from "@/config";
 
 export const SOCIALS = [
