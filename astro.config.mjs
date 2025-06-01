688b0ef10936f0ebb27993d6788f86eb31139f47
Haluk Yilmaz <haluk.yilmaz@roofstacks.com>
2025-06-01 14:23:57 +0300

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import content from '@astrojs/content';
 
 export default defineConfig({
   site: 'https://ainfraboost.com',
+  integrations: [content()],
 });
