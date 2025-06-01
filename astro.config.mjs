706761120f6a6d10a7eebb9206ec9934ba0e5edb
Haluk Yilmaz <haluk.yilmaz@roofstacks.com>
2025-06-01 17:31:53 +0300

    added  astro-paper model new

--- astro.config.mjs
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://ainfraboost.com',
-  output: 'static',
-});
--- src/content/blog/hello-world.md
+++
@@ -1,7 +0,0 @@
----
-title: "Hello World"
-description: "Bu benim ilk yazım"
-pubDate: "2025-06-01"
----
-
-Bu yazı Ainfraboost blog sistemiyle yazıldı 🚀
